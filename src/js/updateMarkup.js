53f964ac5edbf2d80d765bb44333e5ba8a5ae45d
Katerina Zamiatina <eozamyatina@gmail.com>
2021-03-09 11:40:02 +0200

    feat: update swiper

--- src/js/updateMarkup.js
+++ src/js/updateMarkup.js
@@ -1,7 +1,7 @@
 import gallery from '../templates/gallery.hbs';
-// import swiper from './components/swiper';
+import swiper from './components/swiper';
 
-// const swiperContainer = document.querySelector('.swiper-container').swiper
+const swiperContainer = document.querySelector('.swiper-container').swiper
 const galleryContainer = document.querySelector('.image-slider');
 
 export default function updateMarkupGallery(data) {
