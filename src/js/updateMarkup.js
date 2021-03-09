b9566df4aca71d51e858e6dd77ba447f8faa3110
Katerina Zamiatina <eozamyatina@gmail.com>
2021-03-09 11:33:37 +0200

    add: rendering main page

---
+++ src/js/updateMarkup.js
@@ -0,0 +1,15 @@
+import galleryTmpl from '../js/templates/gallery.hbs';
+// import swiper from './components/swiper';
+
+// const swiperContainer = document.querySelector('.swiper-container').swiper
+const galleryContainer = document.querySelector('.image-slider');
+
+export default function updateMarkupGallery(data) {
+  // if (!data) {
+  //   return console.log('fuck');
+  // }
+  const markup = galleryTmpl(data);
+  return galleryContainer.insertAdjacentHTML('beforeend', markup);
+}
+
+//  swiper.document.querySelector('.swiper-container').swiper;
