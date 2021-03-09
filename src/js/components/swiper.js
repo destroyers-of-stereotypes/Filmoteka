d7d8e1568439334fdf4aa2c09e4509140bdb11cf
IvettaDashkova <ivettazakonno@gmail.com>
2021-03-10 01:41:33 +0200

    fix: fetch search

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -4,51 +4,72 @@ import 'swiper/swiper-bundle.min.js';
 import SwiperCore, { Navigation, Pagination } from 'swiper/core';
 
 SwiperCore.use([Navigation, Pagination]);
-const swiper = new Swiper('.swiper-container', {
-  spaceBetween: 20,
-  autoHeight: false,
-  slidesPerColumnFill: 'row',
-  // effect: 'fade',
-  // fadeEffect:{
-  //   crossFade: true,
-  // },
-  // slidesPerGroupSkip: 1,
-  // virtualTranslate: false,
-  // watchOverflow: true,
-  // updateOnWindowResize: true,
-  breakpoints: {
-    320: {
-      slidesPerGroupSkip: 1,
-      // slidesPerGroup: 4,
-      slidesPerView: 1,
-      slidesPerColumn: 4,
-      slidesPerRow: 1,
-    },
-    768: {
-      slidesPerGroupSkip: 1,
-      // slidesPerGroup: 6,
-      slidesPerView: 2,
-      slidesPerColumn: 3,
-      spaceBetween: 30,
-      slidesPerRow: 2,
-    },
-    1030: {
-      slidesPerGroupSkip: 1,
-      // slidesPerGroup: 9,
-      slidesPerView: 3,
-      slidesPerColumn: 3,
-      slidesPerRow: 3,
-      spaceBetween: 30,
-    },
-  },
-
+const swiper = new Swiper('.image-container', {
+  
+  speed: 0,
+  simulateTouch: false,
   pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    renderBullet: function (i, className) {
-      return '<span class="' + className + '">' + (i + 1) + '</span>';
-    },
-  },
-});
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (i, className) {
+          return '<span class="' + className + '">' + (i + 1) + '</span>';
+        },
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+      }
+
+})
+// const swiper = new Swiper('.swiper-container', {
+//   //speed: ?
+
+//   autoHeight: false,
+//   slidesPerColumnFill: 'row',
+//   //=====
+//   // effect: 'fade',
+//   // fadeEffect:{
+//   //   crossFade: true,
+//   // },
+//   // slidesPerGroupSkip: 1,
+//   // virtualTranslate: false, 
+//   // watchOverflow: true,
+//   // updateOnWindowResize: true,
+//   //========
+//   breakpoints: {
+//     320: {
+//       slidesPerGroupSkip: 1,
+//       // slidesPerGroup: 4,
+//       slidesPerView: 1,
+//       slidesPerColumn: 4,
+//       slidesPerRow: 1,
+//     },
+//     768: {
+//       slidesPerGroupSkip: 1,
+//       // slidesPerGroup: 6,
+//       slidesPerView: 2,
+//       slidesPerColumn: 3,
+//       spaceBetween: 30,
+//       slidesPerRow: 2,
+//     },
+//     1030: {
+//       //slidesPerGroupSkip: 1,
+//        slidesPerGroup: 3,
+//       // slidesPerView: 3,
+//       slidesPerColumn: 3,
+//       slidesPerRow: 3,
+//       //spaceBetween: 30,
+//     },
+//   },
+
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//     renderBullet: function (i, className) {
+//       return '<span class="' + className + '">' + (i + 1) + '</span>';
+//     },
+//   },
+// });
 
 export default swiper;
