c659a678e6c962165938151eec6e6c12186f819e
Iryna Protsenko <irene.protsenko@gmail.com>
2021-03-10 18:18:13 +0200

    feat: foterModal v1(draft)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import './sass/main.scss';
 
 import './js/apiService';
 import './js/modal';
-// import './js/modal-old';
-import swiper from './js/components/swiper';
 
-const swiperContainer = document.querySelector('.swiper-container').swiper
+import './js/footerModal';
+
+import swiper from './js/components/swiper';
+const swiperContainer = document.querySelector('.swiper-container').swiper;
