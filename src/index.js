7c94473a4b226eb2c788f7e47e205b8ac7a274aa
IvettaDashkova <ivettazakonno@gmail.com>
2021-03-11 22:31:18 +0200

    ivetta trash

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import './sass/main.scss';
 
+
+import './sass/main.scss';
 import './js/apiService';
 import './js/modal';
 // import './js/modal-old';
 import swiper from './js/components/swiper';
 
+
 const swiperContainer = document.querySelector('.swiper-container').swiper
+
+
+
+// import $ from 'jquery';
+// global.jQuery = $;
+// global.$ = $;
+
+// require("paginationjs");
+
+// $('#pagination-container').pagination({
+//     dataSource: [1, 2, 3, 4, 5, 6, 7],
+//     pageSize: 9,
+//     pageRange: 6,
+//     callback: function(data, pagination) {
+//         // template method of yourself
+//         const html = document;
+//         $('#data-container').html(html);
+//     }
+// })
